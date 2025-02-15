2e6e0af99ca931c7b4591513b0ef4d180a87c722
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2025-02-15 15:03:26 -0300

    Salvar alterações antes de atualizar

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { MatTableModule } from '@angular/material/table';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
+
 
 
 
@@ -14,7 +16,7 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,20 +1,24 @@
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   standalone: false,
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    {_id:'1',name:'angular', category:'front-end'}
-  ];
+
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
 
+  constructor(private coursesService: CoursesService) {
+
+    this.courses = this.coursesService.list();
   }
+
  ngOnInit(): void {}
 }
